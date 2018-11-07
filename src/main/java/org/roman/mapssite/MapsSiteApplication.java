1d4954b277d5ef7e9b2576a50e1073094abf9b00
RMuskovets <rmuskovets@gmail.com>
2018-11-07 18:33:01 +0200

    Initial commit

---
+++ src/main/java/org/roman/mapssite/MapsSiteApplication.java
@@ -0,0 +1,12 @@
+package org.roman.mapssite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapsSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapsSiteApplication.class, args);
+    }
+}
