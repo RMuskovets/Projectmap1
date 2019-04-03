f3ccd5a070123062ef5a7bbd95e39ae91f27893b
RMuskovets <rmuskovets@gmail.com>
2019-04-03 20:20:14 +0300

    add jQuery ($.js), API tools (apitools/api.js) and placeholder for Users page

---
+++ src/main/resources/static/apitools/api.js
@@ -0,0 +1,27 @@
+let _exports = exports || module.exports || {};
+
+function apiGet(api, handleFunction=(()=>{}), data_path='_embedded/', base_uri='api') {
+	let url = `http://${window.location.host}/${base_uri}/${api}`;
+
+	function handle(fn, path)  {
+		return function (data) {
+			var json = JSON.parse(data);
+			for (var pathElement of path.split('/')) {
+				json = json[pathElement];
+			}
+
+			for (var dataElement of json) {
+				fn(dataElement);
+			}
+		};
+	}
+
+	$.ajax({
+		url: url,
+		data: '',
+		success: handle(handleFunction, data_path + api),
+		dataType: 'application/json'
+	});
+}
+
+_exports.apiGet = apiGet;
