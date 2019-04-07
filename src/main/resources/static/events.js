8bf18ee6fa93939f528d8bcfcf506b0040c28fd4
RMuskovets <rmuskovets@gmail.com>
2019-04-07 12:11:45 +0300

    events.html

--- src/main/resources/static/events.js
+++ src/main/resources/static/events.js
@@ -1,14 +1,37 @@
-function appendMark(Mark) {
+function formatDate(date) {
+	var year = date.getFullYear();
+	var month= date.getMonth()+1;
+	var day  = date.getDate();
 
-	var Markname = Mark.name;
-	var password = Mark.password;
-	var elementHTML = `<tr><td>${Markname}</td><td>${password}</td></tr>`;
-	var jqElem		= $(elementHTML);
-	$('#tbody').append(jqElem);
+	var hour = date.getHours();
+	var min  = date.getMinutes();
+	var sec  = date.getSeconds();
+
+	return `${hour}:${min}:${sec} ${day}.${month}.${year}`;
+}
+
+function href2id(object) {
+	const href = object._links.self.href;
+	const spl  = href.split('/');
+	const ints = spl[spl.length-1];
+
+	return parseInt(ints);
+}
+
+function appendEvent(evt) {
+	var name = evt.name;
+	var start= evt.start;
+	var end  = evt.end;
+
+	var startDate = formatDate(new Date(start));
+	var endDate   = formatDate(new Date(end));
+
+	var tr = $(`<tr><td>${href2id(evt)}</td><td>${name}</td><td>${startDate}</td><td>${endDate}</td></tr>`);
+	$('#tbody').append(tr);
 }
 
 $.getJSON("api/events", "", function (data) {
-    $.each(data._embedded.events, function (index, Mark) {
-        appendMark(Mark);
+    $.each(data._embedded.events, function (index, evt) {
+        appendEvent(evt);
     })
 })
