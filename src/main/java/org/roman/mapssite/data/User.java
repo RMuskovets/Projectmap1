7e3f33f01fd0a5c7757af1f17826fc22f5520b0f
RMuskovets <rmuskovets@gmail.com>
2019-04-24 18:34:02 +0300

    Add some data to the User class and to the users table

--- src/main/java/org/roman/mapssite/data/User.java
+++ src/main/java/org/roman/mapssite/data/User.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Entity;
+import java.util.Date;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY) @Entity
 public class User implements DataClass {
@@ -23,6 +24,20 @@ public class User implements DataClass {
 
     @Column
     private Integer type;
+
+    //region Telemetry data
+        @Column
+        private String email;
+        @Column
+        private String firstName;
+        @Column
+        private String lastName;
+        @Column
+        private Date birthDate;
+        @Column
+        private String phoneNumber;
+
+    //endregion
 
     public String getUsername() {
         return username;
@@ -48,18 +63,60 @@ public class User implements DataClass {
         this.type = type;
     }
 
-    public User(String username, String password, Integer type) {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+
+    public User(String username, String password, Integer type, String email, String firstName, String lastName, Date birthDate, String phoneNumber) {
         this.username = username;
         this.password = password;
         this.type = type;
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public User() {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
     }
 
     public Integer getID() {
@@ -67,21 +124,62 @@ public class User implements DataClass {
     }
 
     public static class Builder {
-        private String n, p;
-        private Integer t;
 
-        public Builder(String n, String p, Integer t) {
+        private static void initAll(Builder b) {
+            b.n = b.p = b.e = b.fn = b.ln = b.pn = "";
+            b.t = 0;
+            b.bd = new Date();
+        }
+
+        private String n = "", p = "";
+        private Integer t;
+        private String e, fn, ln, pn;
+        private Date bd;
+
+        public Builder(String n, String p, Integer t, String e, String fn, String ln, String pn, Date bd) {
+            initAll(this);
+            this.n = n;
+            this.p = p;
+            this.t = t;
+            this.e = e;
+            this.fn = fn;
+            this.ln = ln;
+            this.pn = pn;
+            this.bd = bd;
+        }
+
+        public Builder(String n, String p, Integer t, String e, String fn, String ln, Date bd) {initAll(this);
+            this.n = n;
+            this.p = p;
+            this.t = t;
+            this.e = e;
+            this.fn = fn;
+            this.ln = ln;
+            this.bd = bd;
+        }
+
+        public Builder(String n, String p, Integer t, String e, String fn, String ln) {initAll(this);
+            this.n = n;
+            this.p = p;
+            this.t = t;
+            this.e = e;
+            this.fn = fn;
+            this.ln = ln;
+        }
+
+        public Builder(String n, String p, Integer t) {initAll(this);
             this.n = n;
             this.p = p;
             this.t = t;
         }
 
-        public Builder(String n, String p) {
+        public Builder(String n, String p) {initAll(this);
             this.n = n;
             this.p = p;
         }
 
         public Builder() {
+            initAll(this);
         }
 
         public Builder setName(String n) {
@@ -99,8 +197,33 @@ public class User implements DataClass {
             return this;
         }
 
+        public Builder setEmail(String e) {
+            this.e = e;
+            return this;
+        }
+
+        public Builder setFirstname(String fn) {
+            this.fn = fn;
+            return this;
+        }
+
+        public Builder setLastname(String ln) {
+            this.ln = ln;
+            return this;
+        }
+
+        public Builder setPhoneNumber(String pn) {
+            this.pn = pn;
+            return this;
+        }
+
+        public Builder setBirthDate(Date bd) {
+            this.bd = bd;
+            return this;
+        }
+
         public User build() {
-            return new User(n, p, t);
+            return new User(n, p, t, e, fn, ln, bd, pn);
         }
     }
 }
