8665d67a38e401b72684d3439371efcdd721b10f
RMuskovets <rmuskovets@gmail.com>
2019-07-18 17:47:52 +0300

    Update generate_inserts.py

--- Database/generate_inserts.py
+++ Database/generate_inserts.py
@@ -75,20 +75,20 @@ def generate_user_sql(id, type, *, first_name=None, last_name=None, email=None, 
 	sql_birth_date = birth_date or gen_birth_date()
 	sql_username   = gen_username(sql_first_name, sql_last_name).lower()
 	sql_email      = email      or gen_email(sql_username).lower()
-	return f'INSERT INTO user (id, type, first_name, last_name, email, phone_number, password, birth_date, username) VALUES ({id}, {type}, "{sql_first_name}", "{sql_last_name}", "{sql_email}", "{sql_phone_number}", "{sql_password}", {sql_birth_date}, "{sql_username}")'
+	return f'INSERT INTO USER (id, type, first_name, last_name, email, phone_number, password, birth_date, username) VALUES ({id}, {type}, "{sql_first_name}", "{sql_last_name}", "{sql_email}", "{sql_phone_number}", "{sql_password}", {sql_birth_date}, "{sql_username}")'
 
 def generate_place_sql(id, userid_max, *, name=None, coordx=None, coordy=None, owner_id=None):
 	sql_name = name or gen_first_name()
 	sql_cx   = coordx or gen_mark_cx()
 	sql_cy   = coordy or gen_mark_cy()
 	sql_oid  = owner_id or random.randint(0, userid_max)
-	return f'INSERT INTO mark (id, owner_id, name, coordx, coordy) VALUES ({id}, {sql_oid}, "{sql_name}", {sql_cx}, {sql_cy})'
+	return f'INSERT INTO MARK (id, owner_id, name, coordx, coordy) VALUES ({id}, {sql_oid}, "{sql_name}", {sql_cx}, {sql_cy})'
 
 def generate_event_zaprosheni_sql(eid, muid):
 	zapr_len = random.randint(5, 25)
 	s = []
 	for _ in range(zapr_len):
-		s.append(f'INSERT INTO event_zaprosheni (event_id, zaprosheni_id) VALUES ({eid}, {random.randint(0, muid)});')
+		s.append(f'INSERT INTO EVENT_ZAPROSHENI (event_id, zaprosheni_id) VALUES ({eid}, {random.randint(0, muid)});')
 	return '\n'.join(s)
 
 def generate_event_sql(id, userid_max, placeid_max, end=None, start=None, name=None):
@@ -99,7 +99,7 @@ def generate_event_sql(id, userid_max, placeid_max, end=None, start=None, name=N
 
 	sql_zapr = generate_event_zaprosheni_sql(id, userid_max)
 
-	return f'INSERT INTO event (id, end_time, name, start_time, `where`) VALUES ({id}, {sql_end}, "{sql_name}", {sql_start}, {sql_pid});\n{sql_zapr}'
+	return f'INSERT INTO EVENT (id, end_time, name, start_time, `where`) VALUES ({id}, {sql_end}, "{sql_name}", {sql_start}, {sql_pid});\n{sql_zapr}'
 
 def generate_inserts():
 	s = []
